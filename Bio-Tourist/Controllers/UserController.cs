a4833437b2d87be4b587c1d098e8cf63a63dffc3
Benjamin Renard <benjamin.renard91@outlook.fr>
2020-01-24 10:56:28 +0100

    insciption + connection

--- Bio-Tourist/Controllers/UserController.cs
+++ Bio-Tourist/Controllers/UserController.cs
@@ -10,11 +10,12 @@ namespace Bio_Tourist.Controllers
 {
     public class UserController : Controller
     {
-        [HttpGet]
+        public ActionResult Inscription()
+        {
+            return View();
+        }
         public ActionResult Connection()
         {
-            ViewBag.Message = "Your connection page.";
-
             return View();
         }
 
@@ -22,19 +23,20 @@ namespace Bio_Tourist.Controllers
         SqlCommand ConnectionCommand = new SqlCommand();
         SqlDataReader ConnectionDataReader;
 
+        [HttpGet]
         void GetConnectionPath()
         {
             ConnectionPath.ConnectionString = "data source = MSI\\SQLEXPRESS; Database=BIO_TEST; integrated security = SSPI";
         }
 
         [HttpPost]
-        public ActionResult Verify(Models.UserTest Acc)
+        public ActionResult Verify(Models.UserTest p)
         {
             GetConnectionPath();
             ConnectionPath.Open();
 
             ConnectionCommand.Connection = ConnectionPath;
-            ConnectionCommand.CommandText = "SELECT * FROM T_User WHERE Username ='" + Acc.Username + "' AND Password='" + Acc.Password + "'";
+            ConnectionCommand.CommandText = "SELECT * FROM T_User WHERE Username ='" + p.Username + "' AND Password='" + p.Password + "'";
             ConnectionDataReader = ConnectionCommand.ExecuteReader();
 
             if (ConnectionDataReader.Read())
@@ -46,6 +48,53 @@ namespace Bio_Tourist.Controllers
             {
                 ConnectionPath.Close();
                 return View("ConnectionError");
+            }
+        }
+
+
+
+        [HttpPost]
+        public Boolean CheckExist(Models.UserTest p)
+        {
+            GetConnectionPath();
+            ConnectionPath.Open();
+            SqlCommand CheckCommand = new SqlCommand();
+            CheckCommand.Connection = ConnectionPath;
+            CheckCommand.CommandText = "SELECT * FROM T_User WHERE Username ='" + p.Username + "'";
+            ConnectionDataReader = CheckCommand.ExecuteReader();
+            
+            if (ConnectionDataReader.Read())
+            {
+                ConnectionPath.Close();
+                return true;
+            }
+            else
+            {
+                ConnectionPath.Close();
+                return false;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Register(Models.UserTest p)
+        {
+            SqlCommand RegisterCommand = new SqlCommand();
+
+            if (CheckExist(p) == true)
+            {
+                ConnectionPath.Close();
+                return View("InscriptionERROR");
+            }
+            else
+            {
+                GetConnectionPath();
+                ConnectionPath.Open();
+                RegisterCommand.Connection = ConnectionPath;
+                RegisterCommand.CommandText = "INSERT INTO T_User(Username, Password) VALUES('"+ p.Username +"', '"+ p.Password +"' ) ";          
+                RegisterCommand.ExecuteNonQuery();
+
+                ConnectionPath.Close();
+                return View("InscriptionOK");
             }
         }
     }
