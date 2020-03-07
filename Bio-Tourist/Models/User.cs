883bfe00eb78e5e6486f89f359288e132f68efec
thao93 <clement.monges@hotmail.com>
2020-03-07 12:58:13 +0100

    Récupération de la branche de ben

--- Bio-Tourist/Models/User.cs
+++ Bio-Tourist/Models/User.cs
@@ -51,22 +51,6 @@ namespace Bio_Tourist.Models
 
             public List<User> ProfileModel { get; set; }
 
-
-        public int ID_USER { get; set; }
-        public string LAST_NAME_USER { get; set; }
-        public string FIRST_NAME_USER { get; set; }
-        public int AGE_USERS { get; set; }
-        public int NUM_STREET { get; set; }
-        public string NAME_STREET { get; set; }
-        public int POSTAL_CODE { get; set; }
-        public string CITY_USER { get; set; }
-        public string COUNTRY_USER { get; set; }
-        public string EMAIL_USER { get; set; }
-        public int NUM_USER { get; set; }
-        public string PASSWORD_USER { get; set; }
-        public string CONFIRMING_PASSWORD { get; set; }
-        public string CIVILITY_USER { get; set; }
-
         // Pour la page contact = A ne pas toucher
   
         public string TO { get; set; }
